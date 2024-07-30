be4d0d7c5bdd939f78e00a7c6853f73a961dfb2a
tj-messi <2715978522@qq.com>
2024-07-30 14:32:18 +0800

    Update 7-30-H.cpp

--- 牛客/7-30-H.cpp
+++ 牛客/7-30-H.cpp
@@ -4,10 +4,20 @@ using namespace std;
 #define ll long long
 #define endl '\n' 
 
-
+const int N = 50;
+vector<vector<int>> mp (N);
+int n,m;
+int depth;
 
 void solve()
 {
+	cin>>n>>m;
+	for(int i=1;i<=m;i++)
+	{
+		int x,int y;cin>>x>>y;
+		mp[x].push_back(y);
+		mp[y].push_back(x);
+	}
 	
 }
 
