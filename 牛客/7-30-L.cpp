5b579aa80fb0541a92019ee582262c9d856e24f5
tj-messi <2715978522@qq.com>
2024-07-30 12:27:22 +0800

    Create 7-30-L.cpp

---
+++ 牛客/7-30-L.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std; 
+#define int long long
+#define ll long long
+#define endl '\n' 
+
+const int N = 105;
+int a[N];
+
+void solve()
+{
+	int n;cin>>n;
+}
+
+signed main()
+{
+	ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+	int t;
+	cin>>t;
+	//t=1;
+	while(t--)
+	{
+		solve();
+	}
+	return 0;
+} 
