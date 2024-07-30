cc24529e03a3f66a03dee8d001d6d4c7fc022323
tj-messi <2715978522@qq.com>
2024-07-30 10:21:42 +0800

    Update p1032.cpp

--- 洛谷/p1032.cpp
+++ 洛谷/p1032.cpp
@@ -4,11 +4,37 @@ using namespace std;
 #define ll long long
 #define endl '\n' 
 
+const int N = 10;
+string a,b;
+string src[N],dis[N];//规则从src转入dis 
+int n=0;
+struct node
+{
+	string s;
+	int ans;
+};
+map<string,int> mp;
 
+int bfs()
+{
+	queue<node> q;
+	q.push({a,0});
+	mp[a]=1;
+	while(!q.empty())
+	{
+		node now=q.front();q.pop();
+		
+	}
+}
 
 void solve()
 {
-	
+	cin>>a>>b;
+	while(cin>>src[n]>>dis[n])
+	{
+		n++;
+	}
+	//cout<<bfs();
 }
 
 signed main()
