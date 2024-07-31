b777525dce71a2676e0545977f6b54dc88db1068
tj-messi <2715978522@qq.com>
2024-07-31 11:54:05 +0800

    Create p3029.cpp

---
+++ 洛谷/p3029.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std; 
+#define int long long
+#define ll long long
+#define endl '\n' 
+
+const int N = 5e4+5;
+int x[N],id[N];
+int n;
+map<int,int> mp;
+
+void solve()
+{
+	cin>>n;
+	for(int i=1;i<=n;i++)cin>>x[i]>>id[i];
+	for(int i=1;i<=n;i++)
+	{
+		
+	}
+}
+
+signed main()
+{
+	ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+	int t;
+	//cin>>t;
+	t=1;
+	while(t--)
+	{
+		solve();
+	}
+	return 0;
+} 
