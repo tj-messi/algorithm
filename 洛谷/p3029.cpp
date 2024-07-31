80f7bbb742360a08d24b6ee81e20396983ebef96
tj-messi <2715978522@qq.com>
2024-07-31 11:59:38 +0800

    Update p3029.cpp

--- 洛谷/p3029.cpp
+++ 洛谷/p3029.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 const int N = 5e4+5;
 int x[N],id[N];
+vector<int> v;//¿Î…¢ªØ π”√ 
 int n;
 map<int,int> mp;
 
@@ -15,8 +16,12 @@ void solve()
 	for(int i=1;i<=n;i++)cin>>x[i]>>id[i];
 	for(int i=1;i<=n;i++)
 	{
-		
+		v.push_back(x[i]);
+		mp[id[i]]++;
 	}
+	sort(v.begin(),v.end());
+	v.erase(unique(v.begin(),v.end()),v.end());
+	int l=v[0];int r=v[v.size()-1];
 }
 
 signed main()
