b1689739d719f6b02ec93b0103680ed6279562c7
tj-messi <2715978522@qq.com>
2024-08-04 22:51:58 +0800

    1

--- cf/round963-div2/1998C.cpp
+++ cf/round963-div2/1998C.cpp
@@ -4,25 +4,10 @@ using namespace std;
 #define ll long long
 #define endl '\n' 
 
-const int N = 10;
-int ans[N];
 
 void solve()
 {
-	int n;cin>>n;
-	memset(ans,0,sizeof ans);
-	for(int i=1;i<=4*n;i++)
-	{
-		char now;cin>>now;
-		if(now=='?')continue;
-		ans[now-'A'+1]++;
-	}
-	int res=0;
-	for(int i=1;i<=4;i++)
-	{
-		res+=min(ans[i],n);
-	}
-	cout<<res<<endl;
+
 }
 
 signed main()
