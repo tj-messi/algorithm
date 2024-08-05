07bb09615f7986172b0f1110e534cd43d9bcef06
tj-messi <2715978522@qq.com>
2024-08-05 14:56:39 +0800

    Update SPFA单源最短路（负权）.cpp

--- 常用模版/SPFA单源最短路（负权）.cpp
+++ 常用模版/SPFA单源最短路（负权）.cpp
@@ -5,49 +5,21 @@ using namespace std;
 #define endl '\n' 
 
 /*
-dijkstra求单源的最短路，可以有重边，自环。不能有负的边权(这时考虑spfa)，复杂度o(nlogn+m); 
-需要加入根堆优化一下 
+SPFA就是bellman-fold的队列优化版本，负责解决负权图问题，复杂度 
 */
-//const int inf = 10000000ll;
+
+const int inf = 0x3f3f3f3f;
 const int N = 1e5+5;
 int n,m;
 struct node
 {
 	int to,w;
-	//小根堆的优化 
-	bool operator < (const node &v)const
-	{
-		return w==v.w ? to < v.to : w>v.w;
-	}
 };
 vector<node> mp[N];
-int dp[N];int fa[N];//记录每个点的最短路的父亲节点 
-vector<int> ans;//存贮答案 
+int dp[N];//记录最短距离
+int cnt[N];//记录i顶点入队次数，如果大于了n，那么存在负环 
+ 
 
-void dijkstra(int st)
-{
-	for(int i=1;i<=n;i++)
-      dp[i]=9223372036854775806;
-	dp[st]=0;
-	bitset<N> vis;
-	priority_queue<node> pq;
-	pq.push({st,dp[st]});
-	while(!pq.empty())
-	{
-		node now=pq.top();pq.pop();
-		if(vis[now.to])continue;
-		vis[now.to]=true;
-		for(auto next : mp[now.to])
-		{
-			if(!vis[next.to]&&dp[next.to]>dp[now.to]+next.w)
-			{
-				dp[next.to]=dp[now.to]+next.w;
-				pq.push({next.to,dp[next.to]});
-				fa[next.to]=now.to;
-			}
-		}
-	}
-}
 
 void solve()
 {
@@ -58,28 +30,8 @@ void solve()
 		mp[u].push_back({v,w});
 		mp[v].push_back({u,w});
 	}
-	for(int i=1;i<=n;i++)fa[i]=0;
-	dijkstra(1);
-	bool flag=true;
-	int idx=n;
-	while(fa[idx]!=0)
-	{
-		ans.push_back(fa[idx]);
-		if(fa[idx]==1)
-		{
-			flag=false;break;
-		}
-		idx=fa[idx];
-	}
-	if(flag)cout<<-1<<endl;
-	else
-	{
-		for(int i=ans.size()-1;i>=0;i--)
-		{
-			cout<<ans[i]<<" "; 
-		}
-		cout<<n;
-	}
+	int st,ed;cin>>st>>ed;
+	 
 }
 
 signed main()
