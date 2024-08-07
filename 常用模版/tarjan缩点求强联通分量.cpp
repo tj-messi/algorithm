747e7134f371ca8a08bf86b41aa3e64faa028d99
tj-messi <2715978522@qq.com>
2024-08-07 14:53:41 +0800

    1

---
+++ 常用模版/tarjan缩点求强联通分量.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std; 
+#define int long long
+#define ull unsigned long long
+#define endl '\n' 
+
+/*
+tarjan缩点求强联通分量 
+*/
+
+
+void solve()
+{
+	
+}
+
+signed main()
+{
+	ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+	int t;
+	//cin>>t;
+	t=1;
+	while(t--)
+	{
+		solve();
+	}
+	return 0;
+}
