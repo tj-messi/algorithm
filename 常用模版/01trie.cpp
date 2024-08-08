c557ad6f91787fad6189c4d2130ae8e3224f8866
tj-messi <2715978522@qq.com>
2024-08-08 09:13:51 +0800

    1

---
+++ 常用模版/01trie.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std; 
+#define int long long
+#define ull unsigned long long
+#define endl '\n' 
+
+/*
+01trie:经常处理前缀性质问题 
+*/
+
+
+void solve()
+{
+	
+}
+
+signed main()
+{
+	ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+	int t;
+	//cin>>t;
+	t=1;
+	while(t--)
+	{
+		solve();
+	}
+	return 0;
+}
