904f0c795889e05ee88f5752b26578d183f44e14
tj-messi <2715978522@qq.com>
2024-08-09 22:02:38 +0800

    1

--- starrycoding/第k小的异或和.cpp
+++ starrycoding/第k小的异或和.cpp
@@ -15,46 +15,63 @@ struct node
 {
 	int val,son[2];
 };
-node t[N*32];
+node t[N<<5];
 int idx;
+struct Q
+{
+	int id,t,x,k;
+};
+vector<Q> que;
+int ans[N];
 
 void insert(int x)
 {
 	int o=0;
+	t[o].val++;
 	for(int i=30;i>=0;i--)
 	{
-		int y=x>>i&1;
-		int &u=t[o].son[y];
+		int y=x>>i & 1;
+		int &u = t[o].son[y];
 		if(!u)u=++idx;
 		o=u;
+		t[o].val++;
 	}
 }
 
-int getmax(int x)
+int getVal(int x,int k)
 {
-	int o=0;int res=0;
+	int o=0,res=0;
 	for(int i=30;i>=0;i--)
 	{
-		int y=x>>i&1;
-		int u=t[o].son[y],v=t[o].son[!y];
-		if(v)o=v,res|=(1<<i);
-		else o=u;
+		int y=x>>i &1;
+		int u=t[o].son[y];//u是结果小的点，v点的大的点 
+		int v=t[o].son[!y];
+		if(u && t[u].val>=k)o=u;
+		else o=v,k-=u ? t[u].val : 0,res|=1<<i; 
 	}
 	return res;
 }
 
 void solve()
 {
-	int n;cin>>n;
-	for(int i=1;i<=n;i++)cin>>a[i]; 
-	insert(a[1]);
-	int ans=a[1]^a[2];
-	for(int i=2;i<=n;i++)
+	int n,q;cin>>n>>q;
+	for(int i=1;i<=n;i++)cin>>a[i];
+	for(int i=1;i<=q;i++)
 	{
-		insert(a[i]);
-		ans=max(ans,getmax(a[i]));
+		int t,x,k;cin>>t>>x>>k;
+		que.push_back({i,t,x,k});
 	}
-	cout<<ans<<endl;
+	sort(que.begin(),que.end(),[](const Q &u,const Q &v)
+	{
+		return u.t != v.t ? u.t<v.t : u.id < v.id;
+	});
+	int now=0;
+	for(const auto & qe : que)
+	{
+		while(now<qe.t)insert(a[++now]);
+		ans[qe.id] = getVal(qe.x,qe.k);
+	}
+	for(int i=1;i<=q;i++)cout<<ans[i]<<endl;
 }
 
 signed main()
